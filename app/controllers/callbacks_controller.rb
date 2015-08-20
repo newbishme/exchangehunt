5d7cc3f740834ca08c62c0d6eb837681a946d0dc
Jingwen Chen <jin@crypt.sg>
2015-08-20 20:13:52 +0800

    Added devise views, fixed container layout issues, added links to profile/login/signup

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,7 +3,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in @user
-    redirect_to root_path
+    redirect_to user_path(@user.username)
   end
 
 end
