32d2f86d6a944c06e6b0add1fbee3d62344db2e6
Jingwen Chen <jin@crypt.sg>
2015-08-20 20:21:01 +0800

    Compile assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,6 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.action_mailer.default_url_options = { host: 'exchangehunt.herokuapp.com', port: 80 }
 end
