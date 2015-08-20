d3f64dcb355d09ed0fb240c9124b281f116b448f
Jingwen Chen <jin@crypt.sg>
2015-08-20 20:24:30 +0800

    Added precompile filetypes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Exchangehunt
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
   end
 end
