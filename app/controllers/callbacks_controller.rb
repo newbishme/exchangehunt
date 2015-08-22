178cfddc1eab95b3a713c224aa29ec75bedc2476
Jingwen Chen <jin@crypt.sg>
2015-08-22 15:14:03 +0800

    Added WelcomeController + redirect user to Welcome page upon signing up

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,7 +3,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in @user
-    redirect_to user_path(@user.username)
+    redirect_to (@user.username.nil? ? welcome_index_url : root_path)
   end
 
 end
