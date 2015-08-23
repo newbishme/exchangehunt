dbc2dc720df98614fec6452e835610fa4036d666
Jingwen Chen <jin@crypt.sg>
2015-08-24 00:05:37 +0800

    Added Procfile

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,17 @@ ActiveRecord::Schema.define(version: 20150823135053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "institution_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "departments", ["institution_id"], name: "index_departments_on_institution_id", using: :btree
+  add_index "departments", ["user_id"], name: "index_departments_on_user_id", using: :btree
+
   create_table "exchanges", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "department_id"
@@ -70,4 +81,6 @@ ActiveRecord::Schema.define(version: 20150823135053) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "departments", "institutions"
+  add_foreign_key "departments", "users"
 end
