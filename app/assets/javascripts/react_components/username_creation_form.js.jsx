a85836b2656b85cdfab735e0d2eed8375e299832
Jingwen Chen <jin@crypt.sg>
2015-08-27 17:17:44 +0800

    WIP refactor username creation form

--- app/assets/javascripts/react_components/username_creation_form.js.jsx
+++ app/assets/javascripts/react_components/username_creation_form.js.jsx
@@ -37,28 +37,24 @@ var UsernameCreationForm = React.createClass({
   },
 
   validateUsername: function(username) {
-    $(React.findDOMNode(this.refs.createUsernameButton)).addClass("disabled");
 
+    if (username === "") {
+      this.renderBlankUsername("");
+      return;
+    }
+
+    $(React.findDOMNode(this.refs.createUsernameButton)).addClass("disabled");
     var request = new XMLHttpRequest();
+
     request.open('GET', "/users/username/" + username, true);
+
     request.onload = function() {
       if (request.status === 200) {
         this.renderUsernameTaken();
+      } else if (!/^\w+$/.test(username)) {
+        this.renderInvalidCharactersUsername();
       } else {
         this.renderValidUsername();
-
-        if (username === "") {
-          this.renderBlankUsername("");
-          return;
-        }
-        this.renderValidUsername();
-
-        if (!/^\w+$/.test(username)) {
-          this.renderInvalidCharactersUsername();
-          return;
-        }
-        this.renderValidUsername();
-
       }
     }.bind(this);
 
@@ -74,7 +70,6 @@ var UsernameCreationForm = React.createClass({
   handleSubmitButtonClick: function() {
     var username = React.findDOMNode(this.refs.usernameField).value;
     var homeInstitutionEmail = React.findDOMNode(this.refs.homeInstitutionEmailField).value;
-    var exchangeInstitution = React.findDOMNode(this.refs.exchangeInstitutionField).value;
 
     if (username === "") {
       return;
@@ -93,54 +88,41 @@ var UsernameCreationForm = React.createClass({
 
   render: function() {
     return (
-      <div>
-        <div className="row">
-          <br />
-          <h3>Complete your profile</h3>
-          <h5><span>This will be your profile URL: exchangehunt.com/users/</span><span ref="profilePathExample"></span></h5>
-          <h6><span className="red-text hide flow-text" ref="blankUsernameMessage">Username cannot be blank.</span></h6>
-          <h6><span className="red-text hide flow-text" ref="usernameTakenMessage">Username in use, please choose another username.</span></h6>
-          <h6><span className="red-text hide flow-text" ref="invalidCharactersMessage">Username can only contain alphanumeric characters and underscore: A-Z 0-9 _</span></h6>
-          <form className="col s12 m8">
+      <div className="row">
+        <div className="col offset-m2 m8">
+          <form className="row">
             <div className="row">
+              <h3>Complete your profile</h3>
+            </div>
+            <div className="row">
+              <h5><span>This will be your profile URL: exchangehunt.com/users/</span><span ref="profilePathExample"></span></h5>
+              <h6><span className="red-text hide flow-text" ref="blankUsernameMessage">Username cannot be blank.</span></h6>
+              <h6><span className="red-text hide flow-text" ref="usernameTakenMessage">Username in use, please choose another username.</span></h6>
+              <h6><span className="red-text hide flow-text" ref="invalidCharactersMessage">Username can only contain alphanumeric characters and underscore: A-Z 0-9 _</span></h6>
               <div className="input-field">
                 <i className="material-icons prefix">account_circle</i>
-                <input id="icon_prefix" type="text" className="validate" onChange={this.handleTextAreaChange} ref="usernameField"></input>
-                <label htmlFor="icon_prefix" className="flow-text">Username</label>
+                <input id="username_field" type="text" className="validate" onChange={this.handleTextAreaChange} ref="usernameField"></input>
+                <label htmlFor="username_field" className="flow-text">Username</label>
               </div>
             </div>
-          </form>
-        </div>
-        <div className="row">
-          <h5 className="inline">Enter your home institution email</h5>
-          <form className="col s12 m8">
+
             <div className="row">
+              <h5 className="inline">Enter your home institution email</h5>
               <div className="input-field">
                 <i className="material-icons prefix">home</i>
-                <label htmlFor="icon_prefix">Home Institution Email</label>
-                <input id="icon_prefix" type="text" className="validate" ref="homeInstitutionEmailField"></input>
-                <h5><span>Your home institution: </span><span ref="homeInstitutionName"></span></h5>
+                <input id="home_email_field" type="text" className="validate" ref="homeInstitutionEmailField"></input>
+                <label htmlFor="home_email_field">Home Institution Email</label>
               </div>
             </div>
-          </form>
-        </div>
-        <div className="row">
-          <h5 className="inline">Select your exchange institution</h5>
-          <form className="col s12">
-            <div className="row">
-              <div className="input-field col s12">
-                <i className="material-icons prefix">school</i>
-                <label htmlFor="icon_prefix" className="flow-text">Exchange institution email</label>
-                <input id="icon_prefix" type="text" className="validate" ref="exchangeInstitutionField"></input>
-              </div>
+
+            <div className="row center">
+              <button className="btn center waves-effect waves-light btn-large disabled"
+                ref="createUsernameButton"
+                type="submit"
+                onClick={this.handleSubmitButtonClick}><i className="material-icons left">send</i>Create</button>
             </div>
+
           </form>
-        </div>
-        <div className="row">
-          <button className="btn waves-effect waves-light btn-large disabled"
-            ref="createUsernameButton"
-            type="submit"
-            onClick={this.handleSubmitButtonClick}><i className="material-icons left">send</i>Create</button>
         </div>
       </div>
     );
