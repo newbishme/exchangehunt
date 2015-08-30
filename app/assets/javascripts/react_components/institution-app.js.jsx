53ca3ea8e7365019e2e18a601523e56794681f6a
MingYi <bluedabedi@hotmail.com>
2015-08-30 16:42:38 +0800

    Update landing page UI, Attempt to fix React+Materialize+InstitutionApp

--- app/assets/javascripts/react_components/institution-app.js.jsx
+++ app/assets/javascripts/react_components/institution-app.js.jsx
@@ -1,13 +1,10 @@
 var InstitutionApp = React.createClass({
-
-  componentWillMount: function() {
+  componentDidMount: function() {
     var url = "/institutions/" + this.props.institution_id + ".json"
     $.get(url, function(response) {
       this.updateInstitutionObject(response);
     }.bind(this));
-  },
 
-  componentDidMount: function() {
     function initialize() {
       var mapCanvas = document.getElementById('map');
       var mapOptions = {
@@ -37,6 +34,7 @@ var InstitutionApp = React.createClass({
   updateView: function() {
     $(React.findDOMNode(this.refs.loadingWrapper)).addClass("hide")
     $(React.findDOMNode(this.refs.wrapper)).removeClass("hide")
+    Materialize.updateTextFields();
     this.forceUpdate();
     this.loadDisqus();
   },
