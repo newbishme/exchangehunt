1d3b9c3a4aef15420ebee2f4dfc5852d42dca1fc
MingYi <bluedabedi@hotmail.com>
2015-08-31 10:54:22 +0800

    Mailboxer Display first name + last name instead of username

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -10,7 +10,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.username}</option>"
+      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name}</option>"
     end
     s.html_safe
   end
