244a1e45d4370a3ddc99e3a1d2b1727b298ee781
MingYi <bluedabedi@hotmail.com>
2015-09-01 19:06:59 +0800

    Add Myself to chat recipients if there is only Myself
    Minor UI edits

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -10,7 +10,9 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name}</option>"
+      if user != current_user
+        s << "<option value='#{user.id}' #{'selected' if user == chosen_recipient}>#{user.first_name} #{user.last_name}</option>"
+      end
     end
     s.html_safe
   end
