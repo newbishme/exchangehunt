dea87adf340c1ab435e392193a7fb126d13edddf
MingYi <bluedabedi@hotmail.com>
2015-09-01 22:13:51 +0800

    Improve consistency of Messaging feature

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -2,6 +2,6 @@ module ConversationsHelper
   def mailbox_section(title, current_box, opts = {})
     opts[:class] = opts.fetch(:class, '')
     opts[:class] += ' active' if title.downcase == current_box
-    content_tag :li, link_to(title.capitalize, conversations_path(box: title.downcase)), opts
+    content_tag :li, link_to(title.capitalize, conversations_path(box: title.downcase), :class => 'mailboxer-blue-text waves-effect waves-orange btn-flat conversation-nav-buttons center-align'), opts
   end
 end
