44c03aadb3a7c06eaf30a67504bda6b7461d3ff9
Jingwen Chen <jin@crypt.sg>
2015-09-04 20:42:55 +0800

    story in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -143,4 +143,9 @@ class User < ActiveRecord::Base
     friends
   end
 
+  def affliate_with_institution(instn)
+    @graph = Koala::Facebook::API.new(self.oauth_token)
+    @graph.put_connections("me", "exchangehunt:affliate_with", :object => "https://exchangehunt.io/institutions/#{instn.id}")
+  end
+
 end
