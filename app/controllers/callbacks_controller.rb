dfab600da969c9039b5145d9ba299dfd9c0fefed
Jingwen Chen <jin@crypt.sg>
2015-09-05 15:25:00 +0800

    added flash to support page

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,9 +2,15 @@ class CallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    sign_in @user
-    @user.save!
-    redirect_to (@user.username.nil? ? welcome_index_path : root_path)
+    if @user
+      sign_in @user
+      @user.save!
+      redirect_to (@user.username.nil? ? welcome_index_path : root_path)
+    else
+      flash[:error] = "Sorry, we failed to obtain authorization from Facebook. Please fill up the support form below and we'll look into this for you."
+      redirect_to support_path
+    end
+
   end
 
 end
